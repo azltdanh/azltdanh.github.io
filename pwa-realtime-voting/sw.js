ab1db5569573bbbc01178f2f43b0d945c63009b8
dluong-pi <dluong@pingidentity.com>
2018-04-08 22:24:46 +0700

    Initial commit

--- pwa-realtime-voting/sw.js
+++ pwa-realtime-voting/sw.js
@@ -3,7 +3,8 @@ var subdir = '/pwa-realtime-voting';
 var filesToCache = [
     subdir + '/',
     subdir + '/index.html',
-    subdir + '/favicon.ico',
+    subdir + '/assets/favicons/favicon-love.ico',
+    subdir + '/assets/favicons/favicon-chart.ico',
     subdir + '/assets/ratings/emo1.png',
     subdir + '/assets/ratings/emo2.png',
     subdir + '/assets/ratings/emo3.png',
@@ -18,11 +19,11 @@ var filesToCache = [
 
 // install
 self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Install');
+    console.log('[ServiceWorker] install');
     e.waitUntil(
         caches.open(cacheName)
             .then(function (cache) {
-                console.log('[ServiceWorker] Caching app shell');
+                console.log('[ServiceWorker] caching app shell');
                 return cache.addAll(filesToCache);
             })
 
@@ -31,12 +32,12 @@ self.addEventListener('install', function (e) {
 
 // activate
 self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
+    console.log('[ServiceWorker] activate');
     e.waitUntil(
         caches.keys().then(function (keyList) {
             return Promise.all(keyList.map(function (key) {
                 if (key !== cacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
+                    console.log('[ServiceWorker] removing old cache', key);
                     return caches.delete(key);
                 }
             }));
@@ -49,11 +50,46 @@ self.addEventListener('activate', function (e) {
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (response) {
-            if (response) {
-                return response;
-            }
 
-            return fetch(e.request).then(function (response) {
+            // // Cache signature post request
+            // if (event.request.url.includes('updateSignature') && !navigator.onLine) {
+            //     var request = event.request;
+            //     var headers = {};
+            //     for (var entry of request.headers.entries()) {
+            //         headers[entry[0]] = entry[1];
+            //     }
+            //     var serialized = {
+            //         url: request.url,
+            //         headers: headers,
+            //         method: request.method,
+            //         mode: request.mode,
+            //         credentials: request.credentials,
+            //         cache: request.cache,
+            //         redirect: request.redirect,
+            //         referrer: request.referrer
+            //     };
+            //     request.clone().text().then(function(body) {
+            //         serialized.body = body;
+            //         callsToCache.push(serialized);
+            //         console.log(callsToCache);
+            //     });     
+            // }
+            // // Immediately respond if request exists in the cache and user is offline
+            // if (response && !navigator.onLine) {
+            //     return response;
+            // }
+            // // Resubmit offline signature requests
+            // if(navigator.onLine && callsToCache.length > 0) {
+            //     callsToCache.forEach(function(signatureRequest) {
+            //         fetch(signatureRequest.url, {
+            //             method: signatureRequest.method,
+            //             body: signatureRequest.body
+            //         });
+            //     });
+            //     callsToCache = [];
+            // }
+
+            return response || fetch(e.request).then(function (response) {
                 var shouldCache = response.ok;
 
                 if (e.request.method == 'POST') {
