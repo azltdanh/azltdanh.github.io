405cd51deaef35247dd500ba68435c0d8d97c20c
dluong-pi <dluong@pingidentity.com>
2018-04-09 15:32:42 +0700

    Initial commit

--- pwa-realtime-voting/sw.js
+++ pwa-realtime-voting/sw.js
@@ -19,20 +19,18 @@ var filesToCache = [
 
 // install
 self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] install');
+    console.log('[ServiceWorker] installing…');
     e.waitUntil(
-        caches.open(cacheName)
-            .then(function (cache) {
-                console.log('[ServiceWorker] caching app shell');
-                return cache.addAll(filesToCache);
-            })
-
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] caching app shell');
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
 // activate
 self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] activate');
+    console.log('[ServiceWorker] now ready to handle fetches!');
     e.waitUntil(
         caches.keys().then(function (keyList) {
             return Promise.all(keyList.map(function (key) {
@@ -48,62 +46,15 @@ self.addEventListener('activate', function (e) {
 
 // fetch
 self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] now fetch!');
     e.respondWith(
         caches.match(e.request).then(function (response) {
-
-            // // Cache signature post request
-            // if (event.request.url.includes('updateSignature') && !navigator.onLine) {
-            //     var request = event.request;
-            //     var headers = {};
-            //     for (var entry of request.headers.entries()) {
-            //         headers[entry[0]] = entry[1];
-            //     }
-            //     var serialized = {
-            //         url: request.url,
-            //         headers: headers,
-            //         method: request.method,
-            //         mode: request.mode,
-            //         credentials: request.credentials,
-            //         cache: request.cache,
-            //         redirect: request.redirect,
-            //         referrer: request.referrer
-            //     };
-            //     request.clone().text().then(function(body) {
-            //         serialized.body = body;
-            //         callsToCache.push(serialized);
-            //         console.log(callsToCache);
-            //     });     
-            // }
-            // // Immediately respond if request exists in the cache and user is offline
-            // if (response && !navigator.onLine) {
-            //     return response;
-            // }
-            // // Resubmit offline signature requests
-            // if(navigator.onLine && callsToCache.length > 0) {
-            //     callsToCache.forEach(function(signatureRequest) {
-            //         fetch(signatureRequest.url, {
-            //             method: signatureRequest.method,
-            //             body: signatureRequest.body
-            //         });
-            //     });
-            //     callsToCache = [];
-            // }
-
             return response || fetch(e.request).then(function (response) {
-                var shouldCache = response.ok;
-
-                if (e.request.method == 'POST') {
-                    shouldCache = false;
-                }
-
-                if (shouldCache) {
-                    return caches.open(cacheName).then(function (cache) {
-                        cache.put(e.request, response.clone());
-                        return response;
-                    });
-                } else {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[ServiceWorker] caching new fetch');
+                    cache.put(e.request, response.clone());
                     return response;
-                }
+                });
             });
         })
     );
